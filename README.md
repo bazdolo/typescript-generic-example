bddeadb3e43cdaa7d4bc6fa3dbf5352c2ddd3dd4
bazdolo <68475574+bazdolo@users.noreply.github.com>
2023-01-28 17:13:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-generic-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-eyxnui)
---
+++ index.ts
@@ -0,0 +1,53 @@
+function simpleStringState<T>(initial: T): [() => T, (v: T) => void] {
+  let str: T = initial;
+  return [
+    () => str,
+    (v: T) => {
+      str = v;
+    },
+  ];
+}
+
+const [getResult, setResult] = simpleStringState('baz');
+console.log(getResult());
+setResult('yoooo');
+console.log(getResult());
+
+const [getResult1, setResult1] = simpleStringState<string | number>(30);
+console.log(getResult1());
+setResult1('yoooo');
+console.log(getResult1());
+
+type Rank<RankItem> = {
+  item: RankItem;
+  rank: number;
+};
+
+function ranker<RankItem>(
+  items: RankItem[],
+  rank: (v: RankItem) => number
+): RankItem[] {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rank(item),
+  }));
+
+  ranks.sort((a, b) => b.rank - a.rank);
+  return ranks.map((rank) => rank.item);
+}
+
+const chars = [
+  { name: 'baz', hp: 20, atk: 10 },
+  { name: 'bazzer', hp: 20, atk: 20 },
+  { name: 'bazzer2', hp: 20, atk: 30 },
+];
+
+const result = ranker(chars, ({ atk }) => atk);
+
+console.log(result);
+
+const one = new Promise<string>((resolve, reject) => {
+  resolve(45);
+});
+
+one.then((a) => a);
